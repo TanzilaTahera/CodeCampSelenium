786bcb12b73fd54a552af73ca2844652355ef4e4
Tanzila <onlytanzila@gmail.com>
2023-06-27 16:28:56 +1000

    browser test

--- src/test/java/ncs/codecamp/playgroundTests.java
+++ src/test/java/ncs/codecamp/playgroundTests.java
@@ -1,5 +1,30 @@
 package ncs.codecamp;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class playgroundTests {
-    
+
+    @Test
+    public void demoTest(){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://d18u5zoaatmpxx.cloudfront.net/#/");
+
+        driver.findElement(By.id("forename")).sendKeys("Tanzila");
+        driver.findElement(By.id("submit")).click();
+
+        driver.
+
+
+
+
+
+    }
 }
