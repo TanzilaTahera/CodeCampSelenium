7831042fe02d739678c386931ae2a84f5242e2c9
Tanzila <onlytanzila@gmail.com>
2023-06-28 09:00:30 +1000

    assert and xpath

--- src/test/java/ncs/codecamp/playgroundTests.java
+++ src/test/java/ncs/codecamp/playgroundTests.java
@@ -1,10 +1,14 @@
 package ncs.codecamp;
 
+import io.restassured.internal.common.assertion.Assertion;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.jupiter.api.Assertions;
+
+import java.sql.SQLOutput;
 
 public class playgroundTests {
 
@@ -20,9 +24,11 @@ public class playgroundTests {
         driver.findElement(By.id("forename")).sendKeys("Tanzila");
         driver.findElement(By.id("submit")).click();
 
-        driver.
+        String text = driver.findElement(By.xpath("//h1[@class=\"display-1 mb-3\"]")).getText();
 
+        Assertions.assertEquals(text,"Web Playground");
 
+        System.out.println(text);
 
 
 
